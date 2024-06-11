4262d38dcaf7fce177fd480f74c4dbbd22ed629f
Josias Neto <josiasnieto@hotmail.com>
2024-06-11 14:58:29 +0200

    Initial commit

---
+++ game.js
@@ -0,0 +1,153 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animations");
+const ghostsFrames = document.getElementById("ghosts");
+
+let creatRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+}
+
+let fps = 30;
+let blockSize = 20;
+let wallColor = "#342DCA";
+let wallSpaceWidth = blockSize / 1.5;
+let wallOffset = (blockSize - wallSpaceWidth) / 2; 
+let wallInnerColor = "black";
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let gameLoop = () => {
+    update();
+    draw();
+}
+
+let update = () => {
+    //TODO
+
+    pacman.playerMovement();
+}
+
+let draw = () => {
+    creatRect(0, 0, canvas.width, canvas.height, "black")
+    //TODO
+    drawWalls();
+    pacman.draw();
+}
+
+let gameInterval = setInterval(gameLoop, 1000/fps);
+
+let drawWalls = () => {
+    for(let i = 0; i < map.length; i++) {
+        for(let j = 0; j < map[0].length; j++) {
+            if(map[i][j] == 1) {
+                creatRect(
+                 j * blockSize,
+                 i * blockSize, 
+                 blockSize, 
+                 blockSize, 
+                 wallColor
+                );
+
+                if(j > 0 && map[i][j - 1] == 1) {
+                    creatRect(
+                    j * blockSize, 
+                    i * blockSize + wallOffset, 
+                    wallSpaceWidth + wallOffset, 
+                    wallSpaceWidth, 
+                    wallInnerColor
+                );
+                }
+                if(j < map[0].length - 1 && map[i][j + 1] == 1) {
+                    creatRect(
+                        j * blockSize + wallOffset, 
+                        i * blockSize + wallOffset, 
+                        wallSpaceWidth + wallOffset, 
+                        wallSpaceWidth, 
+                        wallInnerColor
+                    );
+                }
+                if(i > 0 && map[i - 1][j] == 1) {
+                    creatRect(
+                    j * blockSize + wallOffset, 
+                    i * blockSize, 
+                    wallSpaceWidth, 
+                    wallSpaceWidth + wallOffset, 
+                    wallInnerColor
+                );
+                }
+                if(i < map.length - 1 && map[i + 1][j] == 1) {   
+                    creatRect(
+                        j * blockSize + wallOffset, 
+                        i * blockSize + wallOffset, 
+                        wallSpaceWidth, 
+                        wallSpaceWidth + wallOffset, 
+                        wallInnerColor
+                    );
+                }
+            }
+        }
+    }
+}
+
+let createNewPacman = () => {
+    pacman = new Pacman(
+        blockSize,
+        blockSize,
+        blockSize,
+        blockSize,
+        blockSize / 5
+    );
+}
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+    let key = event.keyCode;
+
+    setTimeout(() => {
+        if(key == 37 || key == 65) {           //·· Left Movement ·····//
+            pacman.nextDirection = DIRECTION_LEFT;
+
+        } else if (key == 38 || key == 87) {   //·· Up Movement ·······//
+            pacman.nextDirection = DIRECTION_UP;
+
+        } else if (key == 39 || key == 68) {   //·· Right Movement ····//
+            pacman.nextDirection = DIRECTION_RIGHT;
+
+        } else if (key == 40 || key == 83) {   //·· Bottom Movement ···//     
+            pacman.nextDirection = DIRECTION_BOTTOM;
+
+        }
+    }, 1);
+});
